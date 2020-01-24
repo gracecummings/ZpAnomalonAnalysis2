e4900eea6179f4f29c504e236e7f0ca3fafa9045
gracecummings <graceelainecummings@gmail.com>
2020-01-24 14:40:31 +0100

    adding all stackers and optimization plot for one sig sample

---
+++ myStackZpAnomalonHZ_allvars.py
@@ -0,0 +1,193 @@
+import argparse
+import ROOT
+import glob
+import os
+import ConfigParser
+#import numpy as np
+from datetime import date
+from ROOT import kOrange, kViolet, kCyan, kGreen, kPink, kAzure, kMagenta, kBlue
+
+
+def findScale(prodnum,lumi,xsec):
+    expecnum = xsec*lumi
+    scalefac = expecnum/prodnum
+    return scalefac
+
+def nameFall17(histFile):
+    s1 = histFile.split("Fall17.")[1]
+    s2 = s1.split("_TuneCP5")[0]
+    return s2
+
+def nameSignal(histFile):
+    s1 = histFile.split("ZpAnomalonHZ_")[1]
+    s2 = s1.split("_v2")[0]
+    return s2
+
+def massZpsignal(nameSig):#unused
+    s1 = nameSig.split("Zp")[1]
+    s2 = s1.split("-")[0]
+    return int(s2)
+
+def massNDsignal(nameSig):#unused
+    s1 = nameSig.split("ND")[1]
+    s2 = s1.split("-")[0]
+    return int(s2)
+
+def massPoints(nameSig):
+    s1  = nameSig.split("-")
+    mzp = int(s1[0].split("Zp")[1])
+    mnd = int(s1[1].split("ND")[1])
+    mns = int(s1[2].split("NS")[1])
+    return mzp,mnd,mns
+
+def orderFall17DY(histFile):
+    s1 = histFile.split("to")[0]
+    s2 = s1.split("HT-")[1]
+    return int(s2)
+
+def orderFall17TT(histFile):
+    s1 = histFile.split("MiniAOD")[0]
+    s2 = s1.split("_")[-1]
+    return int(s2)
+
+if __name__=='__main__':
+    #build module objects
+    parser = argparse.ArgumentParser()
+    config = ConfigParser.RawConfigParser()
+
+    #Define parser imputs
+    parser.add_argument("-L","--lumi", type=float,default = 137, help = "integrated luminosity for scale in fb^-1")
+    parser.add_argument("-x","--xsec", type=float,help = "desired siganl cross section in fb")
+
+    #Set module options 
+    args = parser.parse_args()
+    config.optionxform = str
+
+    #Get command line parameters
+    lumi = args.lumi
+    sig_xsec = args.xsec
+
+    #Files to stack
+    DYJetsToLL = glob.glob('histsBkg_Commitb1a1547/Fall17.DYJetsToLL_M-50_HT*')#Add more dynamics here
+    TT         = glob.glob('histsBkg_Commitb1a1547/Fall17.TTT*')#Add more dynamics here)
+    #WZTo2L2Q   = glob.glob()
+    #ZZTo2L2Q   = glob.glob()
+    bkgfiles   = [DYJetsToLL,TT]#,WZ,ZZ]
+    bkgnames   = ["DYJetsToLL","TT","WZTo2L2Q","ZZTo2L2Q"]
+    sigfiles   = glob.glob('histsSig_Commitb1a1547/ZpAnomalonHZ_Zp*_v2*')
+
+    #Prep signals
+    sig_colors  = [kOrange,kOrange-3,kCyan,kCyan-6,kGreen,kGreen-6,kPink+7,kPink+4,kViolet+4,kMagenta-2,kMagenta+3]
+    sig_info    = []
+    for s,sig in enumerate(sigfiles):
+        sig_dict = {}
+        sig_dict["tfile"] = ROOT.TFile(sig)
+        sig_samplesize    = sig_dict["tfile"].Get('hnevents').GetBinContent(1)
+        sig_dict["scale"] = findScale(float(sig_samplesize),sig_xsec,lumi)
+        sig_dict["color"] = sig_colors[s]
+        sig_dict["name"]  = nameSignal(sig)
+        mzp,mnd,mns       = massPoints(sig_dict["name"])
+        sig_dict["mzp"]   = mzp
+        sig_dict["mnd"]   = mnd
+        sig_dict["mns"]   = mns
+        sig_info.append(sig_dict)
+
+    #Sort Signals by NS mass, then by ND mass
+    sig_info = sorted(sig_info,key = lambda sig: (sig["mns"],sig["mnd"]))
+
+    #Prep backgrounds
+    fp = open('xsects_2017.ini')
+    config.readfp(fp)
+    bkg_colors = [kAzure-4,kViolet,kAzure-6,kViolet+8,kBlue,kViolet-5,kBlue-8,kViolet+6]
+    bkg_info   = []
+    #Loop through the different bkg processes
+    for b,bkg in enumerate(bkgfiles):
+        bkg_binsum   = {}
+        bkg_binlist  = []
+        bkg_channel  = bkgnames[b]
+        bkg_expyield = 0
+        # bkg xs from .ini file
+        bkgbin_xs_pairs = config.items(bkg_channel)
+        if bkg_channel == "DYJetsToLL":
+            #orders smallest HT to largest
+            bkg.sort(key = orderFall17DY)
+        elif bkg_channel == "TT":
+            #orders smallest # events (xs) to largest
+            bkg.sort(key = orderFall17TT)
+        #loop through each process bin or categrory
+        for s,bkgbin in enumerate(bkg):
+            bkgbin_dict = {}
+            bkgbin_dict["binname"] = bkgbin_xs_pairs[s][0]
+            bkgbin_dict["tfile"]   = ROOT.TFile(bkgbin)
+            bkgbin_sampsize        = bkgbin_dict["tfile"].Get('hnevents').GetBinContent(1)
+            bkgbin_xs              = float(bkgbin_xs_pairs[s][1].split()[0])*1000#Into Femtobarns
+            bkgbin_dict["scale"]   = findScale(float(bkgbin_sampsize),bkgbin_xs,lumi)
+            bkgbin_dict["color"]   = bkg_colors[b]
+            #get the number of passing events
+            bkgbin_yield           = bkgbin_dict["tfile"].Get('hzreco_pt').GetEntries()
+            bkg_expyield          += bkgbin_yield*bkgbin_dict["scale"]
+            bkg_binlist.append(bkgbin_dict)
+            bkg_binsum["expyield"] = bkg_expyield
+            
+            #debug
+            #print nameFall17(bkgbin)
+            #print "the number of events initially ran over ",bkgbin_sampsize
+            #print bkgbin
+            #print bkgbin_xs_pairs[s][0]
+            #print bkgbin_xs_pairs[s][1]
+            #print bkgbin_dict
+            #print "the expected yield ",bkg_binsum["expyield"]
+
+        bkg_binsum["binlist"] = bkg_binlist
+        bkg_binsum["name"]    = bkg_channel
+        bkg_info.append(bkg_binsum)
+
+    #Sort the backgrounds from smallest yields to largest
+    bkg_info = sorted(bkg_info, key = lambda bkg:bkg["expyield"])
+
+    #Get list of included histograms
+    keys = bkg_info[0]["binlist"][0]["tfile"].GetListOfKeys()
+
+    #Make some plots
+    for i,key in enumerate(keys):
+        hname = key.GetName()                                                                      
+        tc = ROOT.TCanvas("tc",hname,600,600)                                                      
+        tc.SetLogy()                                                                               
+        leg = ROOT.TLegend(0.45,0.55,0.90,0.88)
+        hsbkg = ROOT.THStack('hsbkg','')
+        for bkg in bkg_info:
+            #print "************Making Histograms*****************"
+            #print bkg
+            #print "************ended relevant print**************"
+            for b,bkgbin in enumerate(bkg["binlist"]):
+                hbkg = bkgbin["tfile"].Get(hname)
+                hbkg.SetStats(0)
+                hbkg.Scale(bkgbin["scale"])
+                hbkg.SetFillColor(bkgbin["color"])
+                hbkg.SetMaximum(10000000)
+                hbkg.SetMinimum(0.1)
+                hsbkg.Add(hbkg)
+                hsbkg.Draw("HIST")
+                hsbkg.SetMaximum(10000000)
+                hsbkg.SetMinimum(0.1)
+                if b == len(bkg["binlist"])-1:
+                    leg.AddEntry(hbkg,bkg["name"],"f")
+
+        for sig in sig_info:
+            hsig = sig["tfile"].Get(hname)
+            hsig.SetStats(0)
+            hsig.Scale(sig["scale"])
+            hsig.SetLineColor(sig["color"])
+            hsig.SetMaximum(10000000)
+            hsig.SetMinimum(0.1)
+            hsig.Draw("HISTSAME")
+            leg.AddEntry(hsig,sig["name"]+" "+str(sig_xsec/1000)+" pb","l")
+                    
+        ROOT.gStyle.SetLegendBorderSize(0)
+        leg.Draw()
+
+        savdir = str(date.today())
+        if not os.path.exists("stacked_stuff/"+savdir):
+            os.makedirs("stacked_stuff/"+savdir)
+        pngname = "stacked_stuff/"+savdir+"/"+hname+".png" 
+        tc.SaveAs(pngname)
---
+++ myStackZpAnomalonHZ_optimization.py
@@ -0,0 +1,250 @@
+import argparse
+import ROOT
+import glob
+import os
+import ConfigParser
+import numpy as np
+from datetime import date
+from ROOT import kOrange, kViolet, kCyan, kGreen, kPink, kAzure, kMagenta, kBlue
+from math import sqrt
+
+def findScale(prodnum,lumi,xsec):
+    expecnum = xsec*lumi
+    scalefac = expecnum/prodnum
+    return scalefac
+
+def nameFall17(histFile):
+    s1 = histFile.split("Fall17.")[1]
+    s2 = s1.split("_TuneCP5")[0]
+    return s2
+
+def nameSignal(histFile):
+    s1 = histFile.split("ZpAnomalonHZ_UFO-")[1]
+    s2 = s1.split("_v2")[0]
+    return s2
+
+def massZpsignal(nameSig):#unused
+    s1 = nameSig.split("Zp")[1]
+    s2 = s1.split("-")[0]
+    return int(s2)
+
+def massNDsignal(nameSig):#unused
+    s1 = nameSig.split("ND")[1]
+    s2 = s1.split("-")[0]
+    return int(s2)
+
+def massPoints(nameSig):
+    s1  = nameSig.split("-")
+    mzp = int(s1[0].split("Zp")[1])
+    mnd = int(s1[1].split("ND")[1])
+    mns = int(s1[2].split("NS")[1])
+    return mzp,mnd,mns
+
+def orderFall17DY(histFile):
+    s1 = histFile.split("to")[0]
+    s2 = s1.split("HT-")[1]
+    return int(s2)
+
+def orderFall17TT(histFile):
+    s1 = histFile.split("MiniAOD")[0]
+    s2 = s1.split("_")[-1]
+    return int(s2)
+
+if __name__=='__main__':
+    #build module objects
+    parser = argparse.ArgumentParser()
+    config = ConfigParser.RawConfigParser()
+
+    #Define parser imputs
+    parser.add_argument("-L","--lumi", type=float,default = 137, help = "integrated luminosity for scale in fb^-1")
+    parser.add_argument("-x","--xsec", type=float,help = "desired siganl cross section in fb")
+    parser.add_argument("-p","--plot", type=str,help = "desired plot name to make the optimization for")
+
+    #Set module options 
+    args = parser.parse_args()
+    config.optionxform = str
+
+    #Get command line parameters
+    lumi = args.lumi
+    sig_xsec = args.xsec
+    released_plot = args.plot
+
+    #Files to stack
+    DYJetsToLL = glob.glob('histsBkg_Commitb1a1547/Fall17.DYJetsToLL_M-50_HT*_Zpt100WP0*')#Add more dynamics here
+    #TT         = glob.glob('histsBkg_Commitb1a1547/Fall17.TTT*_Zpt100WP0')#Add more dynamics here)
+    #WZTo2L2Q   = glob.glob()
+    #ZZTo2L2Q   = glob.glob()
+    bkgfiles   = [DYJetsToLL]#,TT]#,WZ,ZZ]
+    bkgnames   = ["DYJetsToLL","TT","WZTo2L2Q","ZZTo2L2Q"]
+    sigfiles   = ["histsSig_Commitb1a1547/ZpAnomalonHZ_UFO-Zp2000-ND300-NS1_v2_94X_mc2017_realistic_v3_RA2AnalysisTree_hists_25000Events_Zpt100.0btag0.0mt2g1.0.root"]
+
+    #Prep signals
+    sig_colors  = [kOrange,kOrange-3,kCyan,kCyan-6,kGreen,kGreen-6,kPink+7,kPink+4,kViolet+4,kMagenta-2,kMagenta+3]
+    sig_info    = []
+    for s,sig in enumerate(sigfiles):
+        sig_dict = {}
+        sig_dict["tfile"] = ROOT.TFile(sig)
+        sig_samplesize    = sig_dict["tfile"].Get('hnevents').GetBinContent(1)
+        sig_dict["scale"] = findScale(float(sig_samplesize),sig_xsec,lumi)
+        sig_dict["color"] = sig_colors[s]
+        sig_dict["name"]  = nameSignal(sig)
+        mzp,mnd,mns       = massPoints(sig_dict["name"])
+        sig_dict["mzp"]   = mzp
+        sig_dict["mnd"]   = mnd
+        sig_dict["mns"]   = mns
+        sig_info.append(sig_dict)
+
+    #Sort Signals by NS mass, then by ND mass
+    sig_info = sorted(sig_info,key = lambda sig: (sig["mns"],sig["mnd"]))
+
+    #Prep backgrounds
+    fp = open('xsects_2017.ini')
+    config.readfp(fp)
+    bkg_colors = [kAzure-4,kViolet,kAzure-6,kViolet+8,kBlue,kViolet-5,kBlue-8,kViolet+6]
+    bkg_info   = []
+    #Loop through the different bkg processes
+    for b,bkg in enumerate(bkgfiles):
+        bkg_binsum   = {}
+        bkg_binlist  = []
+        bkg_channel  = bkgnames[b]
+        bkg_expyield = 0
+        # bkg xs from .ini file
+        bkgbin_xs_pairs = config.items(bkg_channel)
+        if bkg_channel == "DYJetsToLL":
+            #orders smallest HT to largest
+            bkg.sort(key = orderFall17DY)
+        elif bkg_channel == "TT":
+            #orders smallest # events (xs) to largest
+            bkg.sort(key = orderFall17TT)
+        #loop through each process bin or categrory
+        for s,bkgbin in enumerate(bkg):
+            bkgbin_dict = {}
+            bkgbin_dict["binname"] = bkgbin_xs_pairs[s][0]
+            bkgbin_dict["tfile"]   = ROOT.TFile(bkgbin)
+            bkgbin_sampsize        = bkgbin_dict["tfile"].Get('hnevents').GetBinContent(1)
+            bkgbin_xs              = float(bkgbin_xs_pairs[s][1].split()[0])*1000#Into Femtobarns
+            bkgbin_dict["scale"]   = findScale(float(bkgbin_sampsize),bkgbin_xs,lumi)
+            bkgbin_dict["color"]   = bkg_colors[b]
+            #get the number of passing events
+            bkgbin_yield           = bkgbin_dict["tfile"].Get('hzreco_pt').GetEntries()
+            bkg_expyield          += bkgbin_yield*bkgbin_dict["scale"]
+            bkg_binlist.append(bkgbin_dict)
+            bkg_binsum["expyield"] = bkg_expyield
+
+        bkg_binsum["binlist"] = bkg_binlist
+        bkg_binsum["name"]    = bkg_channel
+        bkg_info.append(bkg_binsum)
+
+    #Sort the backgrounds from smallest yields to largest
+    bkg_info = sorted(bkg_info, key = lambda bkg:bkg["expyield"])
+
+    #Make the stacked plot
+    hname = released_plot
+    leg = ROOT.TLegend(0.45,0.55,0.90,0.88)
+    hsbkg = ROOT.THStack('hsbkg','')
+    for bkg in bkg_info:
+        for b,bkgbin in enumerate(bkg["binlist"]):
+            hbkg = bkgbin["tfile"].Get(hname)
+            hbkg.SetStats(0)
+            hbkg.Scale(bkgbin["scale"])
+            hbkg.SetFillColor(bkgbin["color"])
+            hbkg.SetMaximum(10000000)
+            hbkg.SetMinimum(0.1)
+            hsbkg.Add(hbkg)
+            #hsbkg.Draw("HIST")
+            hsbkg.SetMaximum(10000000)
+            hsbkg.SetMinimum(0.1)
+            if b == len(bkg["binlist"])-1:
+                leg.AddEntry(hbkg,bkg["name"],"f")
+                
+    #Prep the pads
+    tc = ROOT.TCanvas("tc",hname,600,800)
+    p1 = ROOT.TPad("p1","stack_"+hname,0,0.4,1.0,1.0)
+    #tc.SetLogy()
+    p1.SetLogy()
+    p1.SetBottomMargin(0)
+    p1.SetLeftMargin(0.15)
+    p1.SetRightMargin(0.05)
+    p2 = ROOT.TPad("p2","signif_"+hname,0,0.0,1.0,0.4)
+    #p2.SetTopMargin(0)
+    p2.SetRightMargin(.05)
+    p2.SetLeftMargin(0.15)
+    p2.SetBottomMargin(0.2)
+
+    #Prepare first pad for stack
+    p1.Draw()
+    p1.cd()
+
+    #Draw the stack
+    hsbkg.Draw("HIST")
+
+    #Add the signal plots
+    #mg = ROOT.TMultiGraph()
+
+    for p,masspoint in enumerate(sig_info):
+        #print "def saw ",masspoint["name"]
+        hsig = masspoint["tfile"].Get(hname)
+        hsig.SetStats(0)
+        hsig.Scale(masspoint["scale"])
+        hsig.SetLineColor(masspoint["color"])
+        hsig.SetMaximum(10000000)
+        hsig.SetMinimum(0.1)
+        leg.AddEntry(hsig,masspoint["name"]+" "+str(sig_xsec/1000)+" pb","l")
+        leg.SetBorderSize(0)
+
+        #Add signal curve to stack plot
+        hsig.Draw("HISTSAME")
+        p1.Update()
+        
+        #Now the significance calculation
+        hsum = hsbkg.GetStack().Last()
+        cutlist = np.zeros(hsum.GetNbinsX())
+        signiflist = np.zeros(hsum.GetNbinsX())
+
+        for ibin in range(0,hsum.GetNbinsX()):
+            theocut = hsum.GetBinLowEdge(ibin)
+            bkg     = 0
+            sig     = 0
+            for b in range(ibin,hsum.GetNbinsX()):
+                bkg += hsum.GetBinContent(b)
+                sig += hsig.GetBinContent(b)
+
+            signif           = sig/sqrt(bkg)
+            signiflist[ibin] = signif
+            cutlist[ibin]    = theocut
+
+        print hsum.GetNbinsX()
+        print cutlist
+        print signiflist
+
+        tg = ROOT.TGraph(hsum.GetNbinsX(),cutlist,signiflist)
+        tg.SetTitle("")
+        tg.SetLineWidth(2)
+        tg.SetLineColor(masspoint["color"])
+        tg.GetXaxis().SetTitle("btag value")
+        tg.GetXaxis().SetTitleSize(0.08)
+        tg.GetXaxis().SetLabelSize(0.05)
+        tg.GetYaxis().SetTitle("S/#sqrt{B}")
+        tg.GetYaxis().SetTitleSize(0.08)
+        tg.GetYaxis().SetTitleOffset(.7)
+        tg.GetYaxis().SetLabelSize(0.05)
+        #mg.Add(tg)
+
+        #Make the second pad with the significance plot
+        tc.cd()
+
+        p2.Draw()
+        p2.cd()
+        tg.Draw()
+
+    #Draw the legent with everything added
+    tc.cd()
+    p1.cd()
+    leg.Draw()
+
+    #Save the plot
+    savdir = str(date.today())
+    if not os.path.exists("opt_plots/"+savdir):
+        os.makedirs("opt_plots/"+savdir)
+    pngname = "opt_plots/"+savdir+"/"+hname+"_"+masspoint["name"]+".png" 
+    tc.SaveAs(pngname)
